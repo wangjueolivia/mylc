8f6a16253b2b9754dcd98e14670545091d0cedd8
wangjueolivia <wangjueolivia@users.noreply.github.com>
2016-08-16 23:33:07 -0700

    0816

---
+++ intersection of two arrays.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+       Set<Integer> set=new HashSet();
+        for(int i=0;i<nums1.length;i++){
+           for(int j=0;j<nums2.length;j++){
+               if(nums1[i]==nums2[j]){
+                   set.add(nums1[i]);
+               }
+           }
+        }
+        int[] result=new int[set.size()];
+        int i=0;
+        for(Integer num: set){
+            result[i++]=num;
+        }
+        return result;
+    }
+}
