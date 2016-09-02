15049a6f408aba0b6becf6e3a1a3954ffe799c98
wangjueolivia <wangjueolivia@users.noreply.github.com>
2016-09-01 17:36:10 -0700

    0901

---
+++ intersection of two arrays2.py
@@ -0,0 +1,10 @@
+from collections import Counter
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        a,b=Counter(nums1),Counter(nums2)
+        return list((a & b).elements())
