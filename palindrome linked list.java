31515f98af240c150182534646348cac57c26070
wangjueolivia <wangjueolivia@users.noreply.github.com>
2016-09-21 19:21:00 -0700

    0921

---
+++ palindrome linked list.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        if(fast!=null){
+            slow=slow.next;
+        }
+        slow=reverse(slow);
+        while(slow!=null && head.val==slow.val){
+            head=head.next;
+            slow=slow.next;
+        }
+        return slow==null;
+    }
+    public ListNode reverse(ListNode head){
+        ListNode prev=null;
+        while(head!=null){
+            ListNode next= head.next;
+            head.next=prev;
+            prev=head;
+            head=next;
+        }
+        return prev;
+    }
+}
